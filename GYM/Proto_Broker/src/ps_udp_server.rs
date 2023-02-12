0cae99d41eded809e3a21451f1b454a5ee137a5c
Zankanotachi38 <hugo.mora33@outlook.fr>
2023-02-12 18:29:23 -0500

    Feat: check if a user is already connected

--- GYM/Proto_Broker/src/ps_udp_server.rs
+++ GYM/Proto_Broker/src/ps_udp_server.rs
@@ -1,5 +1,5 @@
 use std::collections::HashMap;
-use std::net::{SocketAddr, UdpSocket};
+use std::net::{IpAddr, SocketAddr, UdpSocket};
 
 use crate::ps_datagram_structs::{MessageType, RQ_Connect_ACK_OK};
 
@@ -39,6 +39,7 @@ impl Server {
             }
         }
     }
+
     fn get_new_id(&mut self) -> u64 {
         let id = self.last_id + 1;
         self.last_id = id;
@@ -49,6 +50,16 @@ impl Server {
         println!("recieved invalid packet from {}", src.ip())
     }
 
+    fn already_connected(&self, ip : &IpAddr, port: u16) -> bool {
+
+        for (key, value) in &self.hashmap {
+            if ip == &value.ip() && port == value.port() {
+                return true;
+            }
+        }
+        return false;
+    }
+
     pub fn main_loop(&mut self) {
         loop {
             let mut buf = [0; 1024];
@@ -57,6 +68,9 @@ impl Server {
                     println!("Received {} bytes from {}", n, src);
                     match MessageType::from(buf[0]) {
                         MessageType::CONNECT => {
+                            if self.already_connected(&src.ip(), src.port()) {
+                                continue;
+                            }
                             let uuid = self.get_new_id();
                             self.hashmap.insert(uuid, src);
                             let RQ_ConnectACK = RQ_Connect_ACK_OK::new(uuid, 1);
